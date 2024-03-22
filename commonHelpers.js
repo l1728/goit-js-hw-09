b7ef14eecec941caac6ad67499a6ebccc084d844
l1728 <151185369+l1728@users.noreply.github.com>
2024-03-22 20:49:57 +0000

    Deploying to gh-pages from @ l1728/goit-js-hw-09@6974d65bf40bda69d7cfc16d61793c56abb481a7 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/styles-3c4e11fa.js";console.log("Gallery");
+import"./assets/styles-3c4e11fa.js";import{S as i}from"./assets/vendor-870f0eb5.js";new i(".gallery a",{});
 //# sourceMappingURL=commonHelpers.js.map
