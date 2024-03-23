2ae0f9ca8c937e4fe85f063ba2f1ea1c8ecc8d9a
l1728 <cheburashka077@ukr.net>
2024-03-23 13:28:35 +0200

    form-css

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,34 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Отримуємо необхідні елементи форми
     const form = document.querySelector('.feedback-form');
-    const emailInput = form.querySelector('input[name="email"]');
-    const messageInput = form.querySelector('textarea[name="message"]');
-    const localStorageKey = 'feedback-form-state'; // Ключ для локального сховища
+    const emailInput = form.querySelector("input");
+    const messageInput = form.querySelector("textarea");
+    const localStorageKey = "feedback-form-state"; // Ключ для локального сховища
 
     // Перевіряємо, чи є збережені дані у локальному сховищі
     const storedData = localStorage.getItem(localStorageKey);
     if (storedData) {
         // Якщо є збережені дані, заповнюємо поля форми ними
         const { email, message } = JSON.parse(storedData);
-        emailInput.value = email;
-        messageInput.value = message;
+        emailInput.value = email.trim();
+        messageInput.value = message.trim();
     }
 
     // Відстежуємо подію input на формі
-    form.addEventListener('input', (event) => {
-        // Перевіряємо, чи змінено значення в полях email або message
+    form.addEventListener("input", onFormInput);
+
+    // Обробник події submit форми
+    form.addEventListener("submit", onFormSubmit);
+
+    
+    
+    function onFormInput(event) { // Перевіряємо, чи змінено значення в полях email або message
         if (event.target === emailInput || event.target === messageInput) {
             // Записуємо дані у локальне сховище
             const formData = {
                 email: emailInput.value,
                 message: messageInput.value
             };
-            localStorage.setItem(localStorageKey, JSON.stringify(formData));
-        }
-    });
-
-    // Обробник події submit форми
-    form.addEventListener('submit', (event) => {
+        localStorage.setItem(localStorageKey, JSON.stringify(formData));
+        };
+    };
+            
+        
+    function onFormSubmit(event) {
         event.preventDefault(); // Забороняємо стандартну поведінку форми
 
         // Отримуємо дані з полів форми
@@ -46,5 +52,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // Виводимо дані у консоль
         console.log('Form data:', formData);
-    });
+
+    };
 });
