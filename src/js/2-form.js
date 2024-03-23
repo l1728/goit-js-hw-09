97dee57e4656c0b42913f398c9dd68f7c84a2564
l1728 <cheburashka077@ukr.net>
2024-03-23 18:24:03 +0200

    form_done

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -5,14 +5,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const messageInput = form.querySelector("textarea");
     const localStorageKey = "feedback-form-state"; // Ключ для локального сховища
 
+
     // Перевіряємо, чи є збережені дані у локальному сховищі
     const storedData = localStorage.getItem(localStorageKey);
     if (storedData) {
         // Якщо є збережені дані, заповнюємо поля форми ними
         const { email, message } = JSON.parse(storedData);
-        emailInput.value = email.trim();
-        messageInput.value = message.trim();
+        emailInput.value = email;
+        messageInput.value = message;
     }
+   
 
     // Відстежуємо подію input на формі
     form.addEventListener("input", onFormInput);
@@ -22,12 +24,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     
     
+   
     function onFormInput(event) { // Перевіряємо, чи змінено значення в полях email або message
         if (event.target === emailInput || event.target === messageInput) {
             // Записуємо дані у локальне сховище
             const formData = {
-                email: emailInput.value,
-                message: messageInput.value
+                email: emailInput.value.trim(),
+                message: messageInput.value.trim()
             };
         localStorage.setItem(localStorageKey, JSON.stringify(formData));
         };
@@ -42,16 +45,18 @@ document.addEventListener('DOMContentLoaded', () => {
             email: emailInput.value,
             message: messageInput.value
         };
+        // Перевіряємо, чи заповнені обидва поля форми
+        if (formData.email && formData.message) {
+            // Виводимо дані у консоль
+            console.log("Form data:", formData);
+        } else {
+            alert("Будь ласка, заповніть обидва поля форми.")
+        };
+
+        // Очищаємо поля форми
+        event.currentTarget.reset();
 
         // Очищаємо локальне сховище
         localStorage.removeItem(localStorageKey);
-
-        // Очищаємо поля форми
-        emailInput.value = '';
-        messageInput.value = '';
-
-        // Виводимо дані у консоль
-        console.log('Form data:', formData);
-
     };
 });
